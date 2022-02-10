dcbdbf764c211e91b5a992271ca84fd77abcd877
Deivid Willyan <deiviid_@hotmail.com>
2022-02-09 22:38:37 -0300

    remove unsed files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:curso_intermediario/firebase_messaging/custom_firebase_messaging.dart';
 import 'package:curso_intermediario/remote_config/custom_remote_config.dart';
 import 'package:curso_intermediario/remote_config/custom_visible_rc_widget.dart';
-import 'package:curso_intermediario/setup/setup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
--- lib/setup/setup.dart
+++
@@ -1,7 +0,0 @@
-import 'package:curso_intermediario/firebase_messaging/custom_firebase_messaging.dart';
-import 'package:curso_intermediario/remote_config/custom_remote_config.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-class Setup {
-  static initialize() async {}
-}
